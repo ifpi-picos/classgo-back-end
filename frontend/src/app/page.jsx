9ae8697ed9b0b421c9bb4b805b5660873c45a07e
landeilsonveloso <landeilsonveloso2022@gmail.com>
2023-10-03 19:18:27 -0300

    delete

--- frontend/src/app/page.jsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-
-export default function Home() {
-  return (
-    <main className='h-screen w-full flex justify-center items-center'>
-      <h1>Olá!</h1>
-    </main>
-  )
-}
