35055fb48f3dd0a77a8b1a984789579fd440a563
landeilsonveloso <landeilsonveloso2022@gmail.com>
2023-10-03 19:17:22 -0300

    teste

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className='h-screen w-full flex justify-center items-center'>
-      <h1>Olá Mundo!</h1>
+      <h1>Olá!</h1>
     </main>
   )
 }
