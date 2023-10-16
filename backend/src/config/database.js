41bc0cff39ea948aff81326678c8fa92086d8a1a
landeilsonveloso <landeilsonveloso2022@gmail.com>
2023-10-16 14:17:25 -0300

    configuração do banco de dados

---
+++ backend/src/config/database.js
@@ -0,0 +1,14 @@
+import Sequelize from "sequelize"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const dbName = process.env.DATABASE_NAME
+const dbUser = process.env.DATABASE_USER
+const dbPassword = process.env.DATABASE_PASSOWRD
+const host = process.env.HOST
+const dialect = process.env.DIALECT
+
+const database = new Sequelize(dbName, dbUser, dbPassword, {host: host, dialect:dialect})
+
+export default database
