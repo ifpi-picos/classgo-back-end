53bae726a0f642ae0e5c5c3dc996d80f9ba65475
landeilsonveloso <landeilsonveloso2022@gmail.com>
2023-10-16 14:16:33 -0300

    implemnetação do axios e do useState

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,31 +1,48 @@
+"use client"
+
+import axios from "axios"
 import Link from "next/link"
+import { useState } from "react"
 
 export default function LoginForm() {
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+
+
+    const signInUrl = "http://localhost:3000/"
+
+    const signIn = () => {
+        axios
+            .post(signInUrl, {email, password})
+            .then((res) => console.log(res.data))
+            .catch((err) => console.log(err))
+    }
+
     return (
         <form className="w-1/3 h-3/5 bg-blue-500 text-gray-100 font-semibold border-gray-100 border rounded-xl flex justify-center items-center">
             <fieldset className="w-5/6 h-5/6 border border-gray-100 flex flex-col justify-evenly rounded-xl">
                 <legend className="m-auto px-6 py-3 border border-gray-100 rounded-sm">idCurso</legend>
 
                 <div className="flex justify-center">
-                    <span>Login</span>
+                    <span className="text-xl">Login</span>
                 </div>
 
                 <div className="flex flex-col items-center">
                     <div className="w-5/6 mb-5 flex flex-col">
                         <label htmlFor="email" className="mb-3">Email</label>
-                        <input id="email" name="email" type="email" placeholder="Digite seu email" required className="text-gray-800 px-2 py-1 border border-gray-100 rounded-sm"/>
+                        <input className="text-gray-800 px-2 py-1 border border-gray-100 rounded-sm" id="email" name="email" type="email" placeholder="Digite seu email" required onChange={(e) => setEmail(e.currentTarget.value)}/>
                     </div>
 
                     <div className="w-5/6 flex flex-col">
                         <label className="mb-3" htmlFor="password">Senha</label>
-                        <input id="passoword" name="password" type="password" placeholder="Digite sua senha" required className="text-gray-800 px-2 py-1 border border-gray-100 rounded-sm"/>
+                        <input className="text-gray-800 px-2 py-1 border border-gray-100 rounded-sm" id="passoword" name="password" type="password" placeholder="Digite sua senha" required onChange={(e) => setPassword(e.currentTarget.value)}/>
 
-                        <Link className="mt-3 underline flex justify-end" href="/">esqueceu senha?</Link>
+                        <Link className="mt-3 underline flex justify-end" href="/forgotpassword">esqueceu senha?</Link>
                     </div>
                 </div>
 
                 <div className="flex flex-col items-center">
-                    <button className="px-6 py-2 mb-5 border border-gray-100 rounded-sm" type="button">Entrar</button>
+                    <button className="px-7 py-3 mb-5 border border-gray-100 rounded-sm" type="button" onClick={() => {signIn()}}>Entrar</button>
                     <Link className="underline" href="/register">Cadastrar-se</Link>
                 </div>
             </fieldset>
