b88f113bcc3ba686b951f60e6b938ebffba1f71b
landeilsonveloso <landeilsonveloso2022@gmail.com>
2023-10-16 17:01:57 -0300

    criação da tela de cadastro

---
+++ frontend/src/app/signup/page.jsx
@@ -0,0 +1,9 @@
+import RegistrationForm from "@/components/RegistrationForm"
+
+export default function SignInPage () {
+    return (
+        <main className="h-screen w-full flex items-center justify-center">
+            <RegistrationForm/>
+        </main>
+    )
+}
