bbe9f7de7905ecd86ace6646c03112986f64a409
landeilsonveloso <landeilsonveloso2022@gmail.com>
2023-11-09 00:59:08 -0300

    config dialect

--- src/config/database.js
+++ src/config/database.js
@@ -7,8 +7,7 @@ const dbName = process.env.DATABASE_NAME
 const dbUser = process.env.DATABASE_USER
 const dbPassword = process.env.DATABASE_PASSWORD
 const host = process.env.HOST
-const dialect = 'mysql'
-console.log('dialect', dialect)
-const database = new Sequelize(dbName, dbUser, dbPassword, {host: host, dialect: dialect})
+
+const database = new Sequelize(dbName, dbUser, dbPassword, {host: host, dialect: "mysql"})
 
 export default database
