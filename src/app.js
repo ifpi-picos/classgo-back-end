528b1bf4ad769337f26f966675700f6d8883a79e
landeilsonveloso <landeilsonveloso2022@gmail.com>
2023-11-09 20:31:12 -0300

    remoção da url no cors

--- src/app.js
+++ src/app.js
@@ -5,9 +5,7 @@ import router from "./routes/router.js"
 const app = express()
 
 app.use(express.json())
-app.use(cors({
-    "origin": "https://reverse-time-back-end.vercel.app/"
-}))
+app.use(cors())
 app.use(router)
 
 export default app
