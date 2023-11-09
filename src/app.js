d9f4d17d87f2db1b9d90f8d63d1e40ee029edc6a
landeilsonveloso <landeilsonveloso2022@gmail.com>
2023-11-09 18:48:39 -0300

    implementação da url no cors

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ import router from "./routes/router.js"
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    "origin": "https://reverse-time-back-end.vercel.app/"
+}))
 app.use(router)
 
 export default app
