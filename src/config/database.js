4b340fd64b2bea86f303cecf986c73eecd0ebd84
landeilsonveloso <landeilsonveloso2022@gmail.com>
2023-11-09 23:50:23 -0300

    remoção do dialectModule

--- src/config/database.js
+++ src/config/database.js
@@ -8,6 +8,6 @@ const dbUser = process.env.DATABASE_USER
 const dbPassword = process.env.DATABASE_PASSWORD
 const host = process.env.HOST
 
-const database = new Sequelize(dbName, dbUser, dbPassword, {host: host, dialect: "mysql", dialectModule: "mysql2"})
+const database = new Sequelize(dbName, dbUser, dbPassword, {host: host, dialect: "mysql"})
 
 export default database
