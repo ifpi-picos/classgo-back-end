7947f66005717e0ace402a6650d4055aaddbad67
landeilsonveloso <landeilsonveloso2022@gmail.com>
2023-11-10 00:19:22 -0300

    adição do dialectModule

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 import app from "./src/app.js"
 import dotenv from "dotenv"
-import database from "./src/config/database.js"
 
 dotenv.config()
 
 const API_URL = process.env.API_URL
 const PORT = process.env.PORT
-
-//database.sync()
 
 app.listen(PORT, () => console.log(`Server Running on: ${API_URL}`))
