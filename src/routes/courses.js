0917f1ed6255fbb8f29b862284da6d20f4b1792f
landeilsonveloso <landeilsonveloso2022@gmail.com>
2023-11-20 23:53:30 -0300

    ajustes

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -1,10 +1,9 @@
 import { Router } from "express"
 import { create, read } from "../controllers/courses.js"
-import verifyToken from "../middlewares/auth.js"
 
 const courseRouter = Router()
 
-courseRouter.post("/", verifyToken, create)
+courseRouter.post("/", create)
 courseRouter.get("/", read)
 
 export default courseRouter
--- src/routes/router.js
+++ src/routes/router.js
@@ -3,11 +3,12 @@ import authRouter from "./auth.js"
 import courseRouter from "./courses.js"
 import forgotPasswordRouter from "./forgotPassword.js"
 import redefinePasswordRouter from "./redefinePassword.js"
+import verifyToken from "../middlewares/auth.js"
 
 const router = Router()
 
 router.use("/", authRouter)
-router.use("/courses", courseRouter)
+router.use("/courses", verifyToken, courseRouter)
 router.use("/forgotpassword", forgotPasswordRouter)
 router.use("/redefinepassword", redefinePasswordRouter)
 
