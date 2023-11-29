aa47437dbf231d6732ab0c2a973fc916757140b7
landeilsonveloso <landeilsonveloso2022@gmail.com>
2023-11-29 01:44:43 -0300

    ajustes

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -16,17 +16,17 @@ export const findAll = async (_, res) => {
 
 export const update = async (req, res) => {
     const id = req.params.id
-    const {newName, newEmail} = req.body
+    const {name, email} = req.body
 
-    if (!newName) {
+    if (!name) {
         return res.status(400).send("Campo nome obrigatório!")
     }
 
-    else if (!newEmail) {
+    else if (!email) {
         return res.status(400).send("Campo email obrigatório!")
     }
 
-    const user = await User.update({name: newName, email: newEmail}, {where: {id: id}})
+    const user = await User.update({name: name, email: email}, {where: {id: id}})
 
     return res.status(200).send("Usuário atualizado com sucesso!")
 }
