15a9ba73ac4ef11af002ff711fb82d9c5049f3f6
landeilsonveloso <landeilsonveloso2022@gmail.com>
2023-12-12 15:02:16 -0300

    ajustes

--- src/routes/classes.js
+++ src/routes/classes.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
-import { create, destroy, findAll, update } from "../controllers/classes.js"
+import { create, destroy, findAll, findOne, update } from "../controllers/classes.js"
 import verifyToken from "../middlewares/auth.js"
 
 const classRouter = Router()
 
 classRouter.post("/create", verifyToken, create)
 classRouter.get("/", verifyToken, findAll)
+classRouter.get("/:id", verifyToken, findOne)
 classRouter.put("/update/:id", verifyToken, update)
 classRouter.delete("/delete/:id", verifyToken, destroy)
 
