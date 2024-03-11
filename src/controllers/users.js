ec59333aad5a6d0ba139649771614919688f1693
landeilsonveloso <landeilsonveloso@gmail.com>
2024-03-11 17:36:05 -0300

    ajustes

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -195,9 +195,7 @@ export const update = async (req, res) => {
     
         const user = await User.findOne({where: {email: email}})
 
-        console.log(typeof(id), typeof(user.id))
-
-        if (id !== user.id) {
+        if (user && id != user.id) {
             return res.status(400).send("Usuário já cadastrado!")
         }
 
