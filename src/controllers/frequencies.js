e477e9cb27f2d3c7f6b456946b31c6f746925571
landeilsonveloso <landeilsonveloso@gmail.com>
2024-03-25 13:28:57 -0300

    test

--- src/controllers/frequencies.js
+++ src/controllers/frequencies.js
@@ -2,13 +2,19 @@ import Frequency from "../models/frequencies.js"
 
 export const create = async (req, res) => {
     const {studentsId, presencies, lessonId, classId} = req.body
+    console.log(studentsId)
+    console.log(presencies)
+    console.log(lessonId)
+    console.log(classId)
 
     try {
         for (let index = 0; index < studentsId.length; index++) {
             const studentId = studentsId[index]
             const presence = presencies[index]
+            console.log(studentId)
+            console.log[(presence)]
 
-            await Frequency.create({studentId, presence, lessonId, classId}) 
+            await Frequency.create({studentId, presence, lessonId, classId})
         }
 
         return res.status(201).send("Aula registrada com sucesso!")
