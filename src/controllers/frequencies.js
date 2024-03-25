19176d1007bdedbebab54022198e2e3ee605f741
landeilsonveloso <landeilsonveloso@gmail.com>
2024-03-25 15:13:14 -0300

    ajustes

--- src/controllers/frequencies.js
+++ src/controllers/frequencies.js
@@ -2,17 +2,11 @@ import Frequency from "../models/frequencies.js"
 
 export const create = async (req, res) => {
     const {studentsId, presencies, lessonId, classId} = req.body
-    console.log(studentsId)
-    console.log(presencies)
-    console.log(lessonId)
-    console.log(classId)
 
     try {
         for (let index = 0; index < studentsId.length; index++) {
             const studentId = studentsId[index]
             const presence = presencies[index]
-            console.log(studentId)
-            console.log[(presence)]
 
             await Frequency.create({studentId, presence, lessonId, classId})
         }
