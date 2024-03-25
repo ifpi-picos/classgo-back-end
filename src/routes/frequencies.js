da4039e60e8f3534a8f4ec387aac3dddb759a722
landeilsonveloso <landeilsonveloso@gmail.com>
2024-03-25 17:21:35 -0300

    ajustes

--- src/routes/frequencies.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express"
-import { create } from "../controllers/frequencies.js"
-import verifyToken from "../middlewares/auth.js"
-
-const frequencyRouter = Router()
-
-frequencyRouter.use("/create", verifyToken, create)
-
-export default frequencyRouter
