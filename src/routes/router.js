ac63af08ed6e82adf06c7546d89667da2b005ec0
landeilsonveloso <landeilsonveloso@gmail.com>
2024-03-25 17:27:34 -0300

    ajustes

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,6 +1,5 @@
 import { Router } from "express"
 import classRouter from "./classes.js"
-import frequencyRouter from "./frequencies.js"
 import lessonRouter from "./lessons.js"
 import studentRouter from "./students.js"
 import userRouter from "./users.js"
@@ -8,7 +7,6 @@ import userRouter from "./users.js"
 const router = Router()
 
 router.use("/classes", classRouter)
-router.use("/frequencies", frequencyRouter)
 router.use("/lessons", lessonRouter)
 router.use("/students", studentRouter)
 router.use("/users", userRouter)
