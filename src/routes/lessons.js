d4e82bbcda92d703f33ebcd58af6e772d37c946d
landeilsonveloso <landeilsonveloso@gmail.com>
2024-03-28 00:24:17 -0300

    ajustes

--- src/routes/lessons.js
+++ src/routes/lessons.js
@@ -1,12 +1,11 @@
 import { Router } from "express"
-import { create, findAll, findOne, update } from "../controllers/lessons.js"
+import { create, findAll, update } from "../controllers/lessons.js"
 import verifyToken from "../middlewares/auth.js"
 
 const lessonRouter = Router()
 
 lessonRouter.post("/create", verifyToken, create)
 lessonRouter.get("/findAll/:classId", verifyToken, findAll)
-lessonRouter.get("/findOne/:id", verifyToken, findOne)
 lessonRouter.put("/update/:id", verifyToken, update)
 
 export default lessonRouter
