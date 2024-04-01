029fb528a8e156270050a197783167e676e0eaa8
landeilsonveloso <landeilsonveloso@gmail.com>
2024-03-31 23:21:05 -0300

    ajustes

--- src/routes/frequencies.js
+++ src/routes/frequencies.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
-import { findAll } from "../controllers/frequencies.js"
+import { findAll, findOne } from "../controllers/frequencies.js"
 import verifyToken from "../middlewares/auth.js"
 
 const frequencyRouter = Router()
 
-frequencyRouter.get("/findAll/:lessonId", verifyToken, findAll)
+frequencyRouter.get("/findAll/:classId", verifyToken, findAll)
+frequencyRouter.get("/findOne/:lessonId", verifyToken, findOne)
 
 export default frequencyRouter
